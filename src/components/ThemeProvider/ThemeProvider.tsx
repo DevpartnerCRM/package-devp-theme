a08e052e0d312a07cde8aca0d72e7a4b9a18a5c2
out-source-dev-07 <out.source.dev.07@gmail.com>
2025-01-22 23:52:06 +0600

    feat: theme provider updated for localization

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -1,23 +1,43 @@
 import React from "react";
 import { Theme } from "@emotion/react";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ThemeOptions, ThemeProvider, createTheme } from "@mui/material/styles";
 import { typography } from "../../typography";
 import lightModePalette from "../../palette";
 import { components } from "../../component";
 
-const defaultTheme = createTheme({
-  palette: lightModePalette,
-  components: components,
-  typography,
-});
+// const defaultTheme = createTheme({
+//   palette: lightModePalette,
+//   components: components,
+//   typography
+// });
 
 interface Props {
   children: React.ReactNode;
   theme?: Theme;
+  themeProps?: object[];
+  direction?: "ltr" | "rtl";
+  updateDependencies?: any[];
 }
 
-const ConvertUpLeadsTheme: React.FC<Props> = ({ children, theme }) => (
-  <ThemeProvider theme={theme ? theme : defaultTheme}>{children}</ThemeProvider>
-);
+const ConvertUpLeadsTheme: React.FC<Props> = ({
+  children,
+  theme,
+  direction = "ltr",
+  updateDependencies = [],
+  themeProps,
+}) => {
+  const useTheme = React.useMemo(
+    () =>
+      createTheme({
+        palette: lightModePalette,
+        components: components,
+        typography,
+        direction: direction,
+        ...themeProps,
+      }),
+    [direction, ...updateDependencies]
+  );
+  return <ThemeProvider theme={theme ? theme : useTheme}>{children}</ThemeProvider>;
+};
 
 export default ConvertUpLeadsTheme;
